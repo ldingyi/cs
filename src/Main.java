a48bef59be29e55cd89c8dd55727ee3080c4d193
ldingyi <49196987+ldingyi@users.noreply.github.com>
2023-01-19 02:22:53 +0800

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         //现在master分之发生变化了 
-            
+            //测试rebase
+        
     }
 }
